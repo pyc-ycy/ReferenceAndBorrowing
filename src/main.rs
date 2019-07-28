c5f07092987074cb2d417ad8ace0e266cfe89c08
御承扬 <2923616405@qq.com>
2019-07-28 21:55:05 +0800

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
-    println!("The length  of '{}' is {}.", s1, len);
-}
+    let reference_to_nothing = dangle();
 
+    /*let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    let r3 = &mut s; // BIG PROBLEM
+
+    println!("{}, {}, and {}", r1, r2, r3);*/
+
+//    let mut s1 = String::from("hello");
+//    change1(&mut s1);
+//    println!("s1:{}",s1);
+    //change(&s1);
+    /*let len = calculate_length(&s1);
+    println!("The length  of '{}' is {}.", s1, len);*/
+}
+fn dangle() -> &String {
+    let s = String::from("hello");
+
+    &s
+}
+/*fn change1(s: &mut String) {
+    s.push_str(", I'm trying!!!");
+}*/
+/*fn change(s:&String) {
+    s.push_str(", world");
+}*/
+/*
 fn calculate_length(s: &String) -> usize {
     s.len()
-}
+}*/
